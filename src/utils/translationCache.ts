ab96bd0f538163cd3b01ca6deff4a5eb5af824f3
sodikaran82-web <sodikaran82@gmail.com>
2026-04-11 20:14:40 +0530

    Delete src directory

--- src/utils/translationCache.ts
+++
@@ -1,9 +0,0 @@
-const translationCache = new Map<string, any>();
-
-export function getCachedTranslation(hash: string) {
-  return translationCache.get(hash);
-}
-
-export function setCachedTranslation(hash: string, result: any) {
-  translationCache.set(hash, result);
-}
